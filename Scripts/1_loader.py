3013153a9d1839c9cbdbc8b7c19e7c4dc3866c88
YoungCap <young.a.lee@capgemini.com>
2018-02-10 21:21:29 +0000

    loader template

---
+++ Scripts/1_loader.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  9 18:42:36 2018
+
+@author: Young
+"""
+
+# =============================================================================
+# Loader
+# =============================================================================
+
+"""
+Description:
+Loads all training dataset into memory
+"""
+
+
+# Import modules
