57da4b94c0f2b7d17063f7dd156d0cce09522e1b
miacarina <mia.mayer@capgemini.com>
2018-02-19 08:50:43 +0000

    create range submission file

--- Scripts/1_loader.py
+++ Scripts/1_loader.py
@@ -19,18 +19,25 @@ Loads all training dataset into memory
 # Import modules
 import os
 import glob
+import sys
 
 import pandas as pd
+import numpy as np
 
+# Plotting
 import matplotlib.pyplot as plt
+import skimage.io
+
+# Import custom functions
+sys.path.append('/Users/Kaggle/nuclei/Functions')
+from Image_Pre_Processing.Transform import rgb2gray
 
 # Import parallelisation modules
 import multiprocessing
 from joblib import Parallel, delayed
 
 
-import cv2
-
+import random
 
 
 def image_id(full_image_folder_path, idx):
@@ -49,6 +56,14 @@ train_y_path = '/Users/Kaggle/nuclei/Data/stage1_train_labels.csv'
 
 full_image_folder_paths = glob.glob(train_x_dir+'/*')
 
+"""
+random.seed(42)
+all_image_paths = glob.glob(os.path.join(train_x_dir, '*', 'images', '*'))
+test_image_path = random.sample(all_image_paths,1)[0]
+test_image_mask_paths = glob.glob(os.path.join('/'.join(test_image_path.split('/')[:-2]), 'masks', '*'))
+"""
+
+
 #bin boolean numpy array
 #
 #numpy array
@@ -59,24 +74,48 @@ full_image_folder_paths = glob.glob(train_x_dir+'/*')
 #
 
 
-os.path.splitext(
+
+
+
+test_image_path = '/Users/Kaggle/nuclei/Data/stage1_train/89be66f88612aae541f5843abcd9c015832b5d6c54a28103b3019f7f38df8a6d/images/89be66f88612aae541f5843abcd9c015832b5d6c54a28103b3019f7f38df8a6d.png'
+test_image_mask_paths = glob.glob(os.path.join('/'.join(test_image_path.split('/')[:-2]), 'masks', '*'))
+
+
+mask_path = test_image_mask_paths[0]
+mask_image = skimage.io.imread(mask_path)
+
+
+# Plot
+plt.imshow(mask_image, 'gray')
+plt.colorbar()
+plt.show()
+
+#mask_image = np.array([[0,0,0,0],[0,1,0,1],[0,1,1,0],[0,0,1,1]], dtype = 'uint8')
+#
+mask_image = mask_image.T
+
+output = []
+for i in range(0,len(mask_image)):
+    if mask_image[i].sum() != 0:
+        #print(i, mask_image[i])
+        for k in range(len(mask_image)):
+            if mask_image[i][k] !=0:
+                print((i*len(mask_image)+k)+1, mask_image[i][k])
+                output.append((i*len(mask_image)+k)+1)
+                
         
+from itertools import groupby, count
+
+groupby(output, lambda n, c=count(): n-next(c))
+
+
+def as_range(iterable): # not sure how to do this part elegantly
+    l = list(iterable)
+    if len(l) > 1:
+        return '{0} {1}'.format(l[0], len(l))
+    else:
+        return '{0} {1}'.format(l[0], '1')
+
+' '.join(as_range(g) for _, g in groupby(output, key=lambda n, c=count(): n-next(c)))
+
         
-        os.path.basename(full_image_folder_paths[0])
-        
-        
-        )
-
-
-
-from skimage import data, io, filters
-
-mask_path = '/Users/Kaggle/nuclei/Data/stage1_train/0a7d30b252359a10fd298b638b90cb9ada3acced4e0c0e5a3692013f432ee4e9/masks/0adbf56cd182f784ca681396edc8b847b888b34762d48168c7812c79d145aa07.png'
-
-
-j = skimage.io.imread(mask_path)
-
-for i in range(0,len(u)):
-    print(u[i].sum())
-    
-    
